debcaec6016026f6b1bed7b7d95569cb2bf83833
eliudefrancisco14 <eliudepauloquintasf@gmail.com>
2024-02-23 16:28:20 +0100

    Add new files

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxLink to="/">Home</NuxLink>
+                <ul>
+                    <li><NuxLink to="/project">Projectos</NuxLink></li>
+                    <li><NuxLink to="/blog">Blog</NuxLink></li>
+                    <li><NuxLink to="/speak">Speak</NuxLink></li>
+                </ul>
+            </nav>
+        </header>
+
+        <div>
+            <slot />
+        </div>
+
+    </div>
+</template>
+
---
+++ pages/speak.vue
@@ -0,0 +1,3 @@
+<template>
+    Speak
+</template>
